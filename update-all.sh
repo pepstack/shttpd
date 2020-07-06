b4dc0fe32b5933db41ccbabc7bfc21c72172c402
pepstack.com <clark211@qq.com>
2020-07-06 11:21:47 +0800

    update outdir

--- update-all.sh
+++ update-all.sh
@@ -26,30 +26,30 @@ bindir=$_cdir/libshttpd
 
 rm -rf $bindir
 
-mkdir -p $bindir/bin/{win64,win86}
+mkdir -p $bindir/bin/{x64,Win32}
 
 mkdir -p $bindir/include/shttpd
 
-mkdir -p $bindir/lib/{win64,win86}
+mkdir -p $bindir/lib/{x64,Win32}
 
 libtarget=$_cdir/msvc/libshttpd/target
 binexample=$_cdir/msvc/example/target
 binshttpd=$_cdir/msvc/shttpd/target
 
 echo "[1] update libshttpd lib: $libtarget => $bindir/lib"
-cp -r $libtarget/x64/Debug $bindir/lib/win64/
-cp -r $libtarget/x64/Release/ $bindir/lib/win64/
+cp -r $libtarget/x64/Debug $bindir/lib/x64/
+cp -r $libtarget/x64/Release/ $bindir/lib/x64/
 
-cp -r $libtarget/Win32/Debug $bindir/lib/win86/
-cp -r $libtarget/Win32/Release/ $bindir/lib/win86/
+cp -r $libtarget/Win32/Debug $bindir/lib/Win32/
+cp -r $libtarget/Win32/Release/ $bindir/lib/Win32/
 
 echo "[2] update example bin: $binexample => $bindir/bin"
-cp $binexample/x64/Release/example.exe $bindir/bin/win64/
-cp $binexample/Win32/Release/example.exe $bindir/bin/win86/
+cp $binexample/x64/Release/example.exe $bindir/bin/x64/
+cp $binexample/Win32/Release/example.exe $bindir/bin/Win32/
 
 echo "[3] update shttpd bin: $binshttpd => $bindir"
-cp $binshttpd/x64/Release/shttpd.exe $bindir/bin/win64/
-cp $binshttpd/Win32/Release/shttpd.exe $bindir/bin/win86/
+cp $binshttpd/x64/Release/shttpd.exe $bindir/bin/x64/
+cp $binshttpd/Win32/Release/shttpd.exe $bindir/bin/Win32/
 
 echo "[4] update libshttpd include: $_cdir/src => $bindir/include"
 cp $_cdir/src/shttpd.h $bindir/include/shttpd/
